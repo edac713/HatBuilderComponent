928dd98596515732b487aa463440296983575c2b
edac713 <128916836+edac713@users.noreply.github.com>
2023-10-10 19:19:36 -0400

    Update hat-builder-new.js

--- hat-builder-new.js
+++ hat-builder-new.js
@@ -1,7 +1,7 @@
 function handleCollectionClick(event) {
   event.preventDefault();
   console.log("Collection card clicked");
-  // Your logic here
+  // Your logic to show the corresponding carousel will go here
 }
 
 $(".collection").on("click", function(event) {
